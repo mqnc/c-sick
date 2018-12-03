b7f11f105d37453d32316561e0815eefe7cad756
Mirko <mirko.kunze@web.de>
2018-12-03 14:18:28 +0100

    cleaned expression parser generation

--- test/expression.lua
+++ test/expression.lua
@@ -1,8 +1,4 @@
 
-input = "3*-4+5*6++7+1*2*3"
---input = "1*2+3*4+5*6*7"
-input = "11?22:33?44(41):55"
-
 OperatorClasses = {
 	{
 		name = "Scope",
@@ -14,8 +10,8 @@ OperatorClasses = {
 		name = "Access",
 		order = "ltr",
 		operators = {
-			{peg="'(' _ Expression _ ')'", cpp="(<#)(#1)"},
-			{peg="'[' _ Expression _ ']'", cpp="(<#)[#1]"},
+			{peg="'(' ~_ Expression ~_ ')'", cpp="(<#)(#1)"},
+			{peg="'[' ~_ Expression ~_ ']'", cpp="(<#)[#1]"},
 			{peg="'.'", cpp="(<#).#>"},
 			{peg="'->'", cpp="(<#)->#>"}
 		}
@@ -111,7 +107,7 @@ OperatorClasses = {
 		name = "Conditional",
 		order = "rtl",
 		operators = {
-			{peg="'?' _ Conditional _ ':'", cpp="(<#)? (#1):(#>)"}
+			{peg="'?' ~_ Conditional ~_ ':'", cpp="(<#)? (#1):(#>)"}
 		}
 	}, {
 		name = "Throw",
@@ -123,12 +119,6 @@ OperatorClasses = {
 }
 
 
-
-
-
-function dump(obj)
-	print(stringify(obj))
-end
 
 -- turn "(#1)+(#2)" into {"(", 1, ")+(", 2, ")"} and infer unaries and binaries from operators
 
@@ -175,8 +165,6 @@ end
 grammar = {}
 actions = {}
 
-print(input)
-
 debuglog = false
 packrat = not debuglog
 
@@ -185,8 +173,7 @@ function rule(r)
 end
 
 rule([[Expression <- ]] .. OperatorClasses[#OperatorClasses].name)
-rule([[Atomic <- '(' _ Expression _ ')' / '[' _ Expression _ ']' / [0-9]*]])
-rule([[_ <- ' '*]])
+rule([[Atomic <- '(' ~_ Expression ~_ ')' / [0-9]*]])
 
 function choice(tbl)
 	local result = ""
@@ -206,47 +193,34 @@ function choice(tbl)
 	return result
 end
 
-function stripWs(tbl)
-	local result={}
-
-	for i, field in ipairs(tbl) do
-		if not field.ws then		
-			table.insert(result, field)
-		end
-	end
-
-	return result
-end
 
 function ltrOperation(params)
 	--print(params.rule .. ": " .. stringify(params))
 
-	vals = stripWs(params.values)
-
-	local result = vals[1]
+	local result = params.values[1]
 
 	local i = 2
 
-	while i <= #vals do
+	while i <= #params.values do
 
 		local raw = ""
 
-		for is, snippet in ipairs(vals[i].cpp) do
+		for is, snippet in ipairs(params.values[i].cpp) do
 			if type(snippet) == "number" then
 				if snippet == -1 then
 					raw = raw .. result
 				elseif snippet == -2 then
-					raw = raw .. vals[i+1]
+					raw = raw .. params.values[i+1]
 				else
-					raw = raw .. vals[i].args[snippet]
+					raw = raw .. params.values[i].args[snippet]
 				end
 			else
 				raw = raw .. snippet
 			end
 		end
-		if vals[i].typ == "u" then
+		if params.values[i].typ == "u" then
 			i = i+1
-		elseif vals[i].typ == "b" then
+		elseif params.values[i].typ == "b" then
 			i = i+2
 		else
 			error("invalid operator type")
@@ -261,32 +235,30 @@ end
 function rtlOperation(params)
 	--print(params.rule .. ": " .. stringify(params))
 
-	vals = stripWs(params.values)
+	result = params.values[1]
 
-	result = vals[1]
-
-	local i = #vals-1
+	local i = #params.values-1
 
 	while i >= 1 do
 
 		local raw = ""
 
-		for is, snippet in ipairs(vals[i].cpp) do
+		for is, snippet in ipairs(params.values[i].cpp) do
 			if type(snippet) == "number" then
 				if snippet == -2 then
 					raw = raw .. result
 				elseif snippet == -1 then
-					raw = raw .. vals[i-1]
+					raw = raw .. params.values[i-1]
 				else
-					raw = raw .. vals[i].args[snippet]
+					raw = raw .. params.values[i].args[snippet]
 				end
 			else
 				raw = raw .. snippet
 			end
 		end
-		if vals[i].typ == "u" then
+		if params.values[i].typ == "u" then
 			i = i-1
-		elseif vals[i].typ == "b" then
+		elseif params.values[i].typ == "b" then
 			i = i-2
 		else
 			error("invalid operator type")
@@ -298,6 +270,7 @@ function rtlOperation(params)
 	return result
 end
 
+
 for i, v in ipairs(OperatorClasses) do
 	local class = OperatorClasses[i]
 	if i==1 then
@@ -308,75 +281,41 @@ for i, v in ipairs(OperatorClasses) do
 
 	local unaries = choice(class.unaries)
 	local binaries = choice(class.binaries)
-	local operation = ""
-	local ruleset = ""
 
 	local uname = class.name .. "Unary"
 	local bname = class.name .. "Binary"
 
-	local hcltr = ""
-	local hcrtl = ""
-
-	if class.order == "ltr" then
-		hcltr = " _ " .. higherClass .. " _ "
-	elseif class.order == "rtl" then
-		hcrtl = " _ " .. higherClass .. " _ "
-	end
-
 	if unaries ~= "" and binaries == "" then
 		unaries = uname .. " <- " .. unaries .. "\n"
-		operation = operation .. " ( _ " .. uname .. ")*"
+		if class.order == "ltr" then			
+			operation = class.name .. ": ltrOperation <- " .. higherClass .. " ( ~_ " .. uname .. " )*"
+		else
+			operation = class.name .. ": rtlOperation <- ( " .. uname .. " ~_ )* " .. higherClass .. ""
+		end
 	elseif unaries == "" and binaries ~= "" then
 		binaries = bname .. " <- " .. binaries .. "\n"
-		operation = operation .. " ( _ " .. hcrtl .. bname .. hcltr .. " )*"
+		operation = class.name .. ": " .. class.order .. "Operation <- " .. higherClass .. " ( ~_ " .. bname .. " ~_ " .. higherClass .. " )*"
 	elseif unaries ~= "" and binaries ~= "" then
 		unaries = uname .. " <- " .. unaries .. "\n"
 		binaries = bname .. " <- " .. binaries .. "\n"
-		operation = operation .. " ( _ " .. uname .. " / ( _ " .. hcrtl .. bname .. hcltr .. "))*"
+		if class.order == "ltr" then
+			operation = class.name .. ": ltrOperation <- " .. higherClass .. " ( ~_ " .. uname .. " / ( " .. bname .. " ~_ " .. higherClass .. " ) )*"
+		else
+			operation = class.name .. ": rtlOperation <- ( " .. uname .. " / ( " .. higherClass .. " ~_ " .. bname .. " ) ~_ )* " .. higherClass .. ""
+		end
 	end
-
-	operation = class.name .. " <- " .. hcltr .. operation .. hcrtl
-
+	
 	rule(unaries .. binaries .. operation)
 
 	actions[uname] = function(params)
-		return {typ='u', cpp=class.unaries[params.choice].snippet, args=stripWs(params.values)}
+		return {typ='u', cpp=class.unaries[params.choice].snippet, args=params.values}
 	end
 
 	actions[bname] = function(params)
-		return {typ='b', cpp=class.binaries[params.choice].snippet, args=stripWs(params.values)}
-	end
-
-	if class.order == "ltr" then
-		actions[class.name] = ltrOperation
-	elseif class.order == "rtl" then
-		actions[class.name] = rtlOperation
+		return {typ='b', cpp=class.binaries[params.choice].snippet, args=params.values}
 	end
 
 end
 
 print(table.concat(grammar, "\n"))
 
-function default(params)
-	--dump(params)
-	return params.matched
-end
-
-actions["_"] = function(params)
-	return {["ws"] = true}
-end
-
-actions["Expression"] = function(params)
-	return params.values[1]
-end
-
-pp = pegparser{
-	grammar = table.concat(grammar, "\n"),
-	actions = actions,
-	default = default,
-	packrat = packrat,
-	debuglog = debuglog}
-
-output = pp:parse(input)
-
-print(output)
