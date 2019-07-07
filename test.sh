d9d993c78826b82b1a9e7a5cdb4fe2398ba53956
Mirko <mirko.kunze@web.de>
2019-07-07 16:58:46 +0200

    running through all files in snippet dir

--- test.sh
+++ test.sh
@@ -1,4 +1,26 @@
-./test/cinnamon language/cinnamon.lua language/snippets/helloworld.mon test/helloworld.cpp \
-&& cp -f language/cinnamon.h test/ \
-&& g++ -o test/helloworld -std=c++17 -Wall -Wextra -g -pedantic -pthread test/helloworld.cpp \
-&& test/helloworld
+
+cp -f language/cinnamon.h test/
+
+FILES=language/snippets/*
+
+echo $FILES
+
+for INFILE in $FILES
+do
+	FILEMON=`echo $INFILE | sed 's/language\/snippets\///'`
+	FILE=`echo $FILEMON | sed 's/.mon//'`
+
+	rm test/$FILE
+	./test/cinnamon language/cinnamon.lua language/snippets/$FILE.mon test/$FILE.cpp \
+	&& g++ -o test/$FILE -std=c++17 -Wall -Wextra -g -pedantic -pthread test/$FILE.cpp
+done
+
+echo "- - - - - - - - - - 8< - - - - - - - - - -"
+
+for INFILE in $FILES
+do
+	FILEMON=`echo $INFILE | sed 's/language\/snippets\///'`
+	FILE=`echo $FILEMON | sed 's/.mon//'`
+
+	test/$FILE
+done
