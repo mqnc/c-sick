99e431c78cea9355fc4a158712c62965771a8fae
Mirko <mirko.kunze@web.de>
2019-08-25 14:25:37 +0200

    restructured testing

---
+++ debug.sh
@@ -0,0 +1,2 @@
+echo "test/cinnamon language/cinnamon.lua && pegdebug test/testgrammar.peg test/test.mon test/debug.html && chromium-browser test/debug.html"
+test/cinnamon language/cinnamon.lua && pegdebug test/testgrammar.peg test/test.mon test/debug.html && chromium-browser test/debug.html
---
+++ run_tests.sh
@@ -0,0 +1,25 @@
+
+./test/cinnamon language/cinnamon.lua
+
+cp -f language/libs/* test/
+
+FILES=language/snippets/*
+
+echo $FILES
+
+for INFILE in $FILES
+do
+	echo "\n- - - - - - - - - -8< - - - - - - - - -\n"
+
+	FILEMON=`echo $INFILE | sed 's/language\/snippets\///'`
+	FILE=`echo $FILEMON | sed 's/.mon//'`
+
+	cp -f $INFILE test/
+	rm test/$FILE
+	./test/cinnamon language/cinnamon.lua test/$FILE.mon test/$FILE.cpp \
+	&& g++ -o test/$FILE -std=c++17 -Wall -Wextra -g -pedantic -pthread test/$FILE.cpp
+
+	echo "\n"
+	test/$FILE
+	echo "\n"
+done
